b3e41a7741701774f6299aae51856cfd7c174d96
Naveen Mustang <naveen352kumar@gmail.com>
2019-01-17 16:20:57 +0530

    Initial commit
    devlopment branch

--- app/src/main/java/com/example/pavinaveen/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pavinaveen/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Hello git", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello devlpment", Toast.LENGTH_SHORT).show();
     }
 }
